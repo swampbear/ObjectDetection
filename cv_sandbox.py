6560110bb10bdddff871e107ba5539154e5b6123
Jobjorn <672641@stud.hvl.no>
2024-10-10 17:33:56 +0200

    added real time video to sandbox

--- cv_sandbox.py
+++ cv_sandbox.py
@@ -1,5 +1,28 @@
 import cv2 as cv
-img = cv.imread("assets/prog_drawing.png")
+import numpy as np
 
-cv.imshow("Display window", img)
-k = cv.waitKey(0) # Wait for a keystroke in the window
+def dispImage():
+    img = cv.imread("assets/prog_drawing.png")
+    cv.imshow("Display window", img)
+    k = cv.waitKey(0)  # Wait for a keystroke in the window
+
+cap = cv.VideoCapture(2)
+
+if not cap.isOpened():
+    print("Error could not open webcam")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print('Error failed to capture frame')
+        break
+
+    cv.imshow("Camera", frame)
+    # Press 'q' to quit the video window
+    if cv.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
