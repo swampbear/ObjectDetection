455813c69bfc664d2dd4ca6fb60d723f48cda795
Jobjorn <672641@stud.hvl.no>
2024-10-10 23:35:49 +0200

    converted real time video to a function in the sandbox

--- cv_sandbox.py
+++ cv_sandbox.py
@@ -1,28 +1,31 @@
 import cv2 as cv
 import numpy as np
 
-def dispImage():
+def display_image():
     img = cv.imread("assets/prog_drawing.png")
     cv.imshow("Display window", img)
-    k = cv.waitKey(0)  # Wait for a keystroke in the window
+    k = cv.waitKey(0)
 
-cap = cv.VideoCapture(2)
+def disp_real_time_video():
+    cap = cv.VideoCapture(0)
 
-if not cap.isOpened():
-    print("Error could not open webcam")
-    exit()
+    if not cap.isOpened():
+        print("Error could not open webcam")
+        exit()
 
-while True:
-    ret, frame = cap.read()
+    while True:
+        ret, frame = cap.read()
 
-    if not ret:
-        print('Error failed to capture frame')
-        break
+        if not ret:
+            print('Error failed to capture frame')
+            break
 
-    cv.imshow("Camera", frame)
-    # Press 'q' to quit the video window
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        break
+        cv.imshow("Camera", frame)
+        # Press 'q' to quit the video window
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
 
-cap.release()
-cv.destroyAllWindows()
+    cap.release()
+    cv.destroyAllWindows()
+
+disp_real_time_video()
